6cf86fbf6196527f4ccea30c8e9adeed5d3f5a63
Juan Carlos Valdés <jcvaldes@combinatoria.com.ar>
2017-03-31 10:19:00 -0300

    Persistencia con Mongoose

--- routes/movie.js
+++ routes/movie.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var router = express.Router()
+"use strict"
+const express = require('express')
+const router = express.Router()
+const Movie = require('../lib/model/movie')
 const chai = require('chai')
 const _ = require('lodash')
 
-global.expect = chai.expect
-var Movie = {}
+
+
 /* GET users listing. */
 
 router.post('/', function(req, res, next) {
@@ -14,21 +16,37 @@ router.post('/', function(req, res, next) {
       .status(403)
       .json({error: true, message: 'Body empty'})
   }
-
   let _movie = req.body
-  _movie._id = Date.now()
-  Movie[_movie._id] = _movie
 
-  res
-    .status(201)
-    .json({movie: Movie[_movie._id]})
+  new Movie({
+    title: _movie.title,
+    year: _movie.year
+  })
+  .save((err, movie) => {
+    if(err){
+      res
+        .status(403)
+        .json({error:true, message: 'params empty'})
+    }
+
+    res
+      .status(201)
+      .json({movie: movie})
+  })
 })
 .get('/', function (req, res, next) {
   console.log("GET: ", req.body)
-  res
-    .status(200)
-    .json({movies: _.values(Movie)})
+  Movie.find({},(err, movies) => {
+    if(err){
+      res
+        .status(403)
+        .json({error:true, message: 'params empty'})
+    }
 
+    res
+      .status(200)
+      .json({movies: movies})
+  })
 })
 
 .get('/:id', function (req, res, next) {
@@ -39,11 +57,18 @@ router.post('/', function(req, res, next) {
       .json({error:true, message: 'params empty'})
   }
 
-  let movie = Movie[req.params.id]
-  res
-    .status(200)
-    .json({movie: movie})
+  let _id = req.params.id
+  Movie.findOne({_id: _id}, (err, movie)=>{
+    if(err){
+      res
+        .status(403)
+        .json({error:true, message: 'params empty'})
+    }
 
+    res
+      .status(200)
+      .json({movie: movie})
+  })
 })
 
 .put('/:id', function (req, res, next) {
@@ -54,16 +79,22 @@ router.post('/', function(req, res, next) {
       .json({error:true, message: 'params empty'})
   }
 
+  let _id = req.params.id
   let newMovie = req.body
-  newMovie._id = parseInt(req.params.id, 10)
 
-  Movie[req.params.id] = newMovie
-  newMovie = Movie[req.params.id]
-
-  res
-    .status(200)
-    .json({movie: newMovie})
-
+  Movie.findByIdAndUpdate(_id, {
+    title: newMovie.title,
+    year: newMovie.year
+  }, {new: true}, (err, movie) =>{
+    if(err){
+      res
+        .status(403)
+        .json({error:true, message: 'params empty'})
+    }
+    res
+      .status(200)
+      .json({movie: movie})
+  })
 })
 
 .delete('/:id', function (req, res, next) {
@@ -74,12 +105,19 @@ router.post('/', function(req, res, next) {
       .json({error:true, message: 'params empty'})
   }
 
-  delete Movie[req.params.id]
+  let _id = req.params.id
+  Movie.findByIdAndRemove(_id, (err, done)=>{
+    if(err){
+      res
+        .status(403)
+        .json({error:true, message: 'params empty'})
+    }
+    res
+      .status(400)
+      .json({})
+  })
 
 
-  res
-    .status(400)
-    .json({})
 
 })
 module.exports = router
